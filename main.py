bb93275f98f24bdec26ffb7409056782647a5d7b
Freeman <eric.asante.freeman@gmail.com>
2025-12-01 11:30:15 +0000

    added front card

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+front_card = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=front_card)
+canvas.grid(row=1, column=1)
+
 window.mainloop()
