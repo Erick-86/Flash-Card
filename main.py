76b6af3ac6fbd768626b720998bc782fef3f4021
Freeman <eric.asante.freeman@gmail.com>
2025-12-01 12:19:26 +0000

    set the card column span to 2 and removed the buttons borders

--- main.py
+++ main.py
@@ -11,17 +11,23 @@ front_card = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=front_card)
 lang_text = canvas.create_text(400, 150, text=f"French", fill="black", justify="center", font=("Arial", 30, "italic"))
 lang_word_text = canvas.create_text(400, 250, text=f"Word", fill="black", justify="center", font=("Arial", 30, "bold"))
-canvas.grid(row=1, column=1)
+canvas.grid(row=1, column=1, columnspan=2)
 
 # Buttons
 # Cancel Btn
 cancel_btn_img = PhotoImage(file="images/wrong.png")
-cancel_btn = Button(image=cancel_btn_img)
+cancel_btn = Button(
+    image=cancel_btn_img,
+    highlightthickness=0,
+    borderwidth=0)
 cancel_btn.grid(row=2, column=1)
 
 # Correct Btn
 correct_btn_img = PhotoImage(file="images/right.png")
-correct_btn = Button(image=correct_btn_img)
+correct_btn = Button(
+    image=correct_btn_img,
+    highlightthickness=0,
+    borderwidth=0)
 correct_btn.grid(row=2, column=2)
 
 window.mainloop()
